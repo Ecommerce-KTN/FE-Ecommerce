98f80ec0132eabc799930375c128d71d96b27dd9
levonhutnguyen369 <levonhutnguyen369@gmail.com>
2024-08-31 23:53:47 +0700

    Add file

--- src/components/manager/product/createProduct/CreateProduct.js
+++ src/components/manager/product/createProduct/CreateProduct.js
@@ -1,18 +1,24 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import ProductImage from './ProductImage';
+import Header from './Header';
+
 function CreateProduct() {
 
   const height = useRef(window.innerHeight).current
   return (
-    <div style={{flexDirection: "column", height:height}}>
-        <div style={{ height: "15%", backgroundColor: "red"}}>
-            <div>TopTitle</div>
+    <div style={{flexDirection: "column", height:height, paddingLeft: "4rem", paddingRight: "3rem"}}>
+        <div style={{ height: "30%", backgroundColor: "", marginTop: "2.8rem"}}>
+            <Header/>
         </div>
         <div style={{display:"flex",flexDirection:"row"}}>
-            <div style={{width:"50%",height:height*0.85, backgroundColor:"blue"}}>
+            <div style={{width:"50%",height:height*0.85, backgroundColor:""}}>
                 <div>text</div>           // Kiệt
             </div>
-            <div style={{width:"50%",height:height*0.85, backgroundColor:"red"}}>
-                <div>AddImage</div>  //nguyên
+            <div style={{width:"50%",height:height*0.85, backgroundColor:""}}>
+                <div>
+                    <h3>Product Images</h3>
+                    <ProductImage/>
+                </div>
             </div>
         </div>
     </div>
