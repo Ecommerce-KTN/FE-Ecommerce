0ea459db1a0d0375afa9593d5c814b8eaa7fc81b
Anh Thu <thuleanh543@gmail.com>
2024-09-10 22:13:53 +0700

    update product detail

---
+++ src/pages/ProductDetail/ProductDetail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Headers from '../../components/Headers/Headers'
+
+function ProductDetail ()
+{
+    return (
+        <div>
+            <Headers />
+        </div>
+    )
+}
+
+export default ProductDetail
