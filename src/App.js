cd9bb2aaa4749ade1f05a0bac9141fabc4f3127d
Anh Thu <thuleanh543@gmail.com>
2024-09-15 17:32:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Category from './components/app/Category';
 import Product from './components/app/Product';
 import { Button } from '@mui/material';
 import Header from './components/app/Header';
+import Footer from './components/app/Footer';
 
-function App ()
-{
+function App () {
   return (
     <div className="">
       <div>
@@ -16,12 +16,13 @@ function App ()
           <Button variant="contained">Manager</Button>
         </Link>
       </div>
-      <div className="border-b border-gray-300"><Header/></div>
-      
+      <div className="border-b border-gray-300"><Header /></div>
+
       <div className="mx-auto w-8/12">
         <EmblaCarousel />
         <Category />
         <Product />
+        <Footer />
       </div>
     </div >
   );
--- src/components/app/Footer.js
+++ src/components/app/Footer.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import { ThemeProvider } from '@emotion/react'
+import { createTheme, TextField } from '@mui/material'
+import { Button } from '@mui/material'
+import React, { useState } from 'react'
 
-function Footer ()
-{
+function Footer () {
+    const [ email, setEmail ] = useState( '' )
     return (
-        <div>Footer</div>
+        <ThemeProvider theme={ theme }>
+            <div className='flex-1 mt-10 mb-10 w-full h-full'>
+                <div className='flex gap-4 w-full h-full'>
+                    <div className='border-solid border-2 border-light-gray-500 w-5/12 rounded-lg font-sans p-5'>
+                        <h3 className='font-bold'>Free gift with your 1st order </h3> Join our newsletter to claim it
+                        <div className='flex gap-2'>
+                            <TextField placeholder='Enter your email'
+                                onChange={ ( e ) => setEmail( e.target.value ) }
+                            />
+                            <Button variant='contained' color='buttonSubcriber' style={ {
+                                borderRadius: '10px', fontWeight: 'bold', fontFamily: 'inherit',
+                                textTransform: 'none'
+                            } }
+
+                            >Subscribe</Button>
+                        </div>
+                    </div>
+                    <div className='border-solid border-2 border-light-gray-500 w-7/12 rounded-lg font-sans p-5'>
+                        new
+                    </div>
+
+                </div>
+            </div>
+        </ThemeProvider>
     )
 }
+
+const theme = createTheme( {
+    palette: {
+        buttonSubcriber: {
+            main: 'hsl(338deg 91% 68% / 1)',
+            light: '#eb83b2',
+            dark: 'hsl(338deg 91% 68% / 1)',
+            text: '#ffffff',
+            contrastText: '#ffffff',
+        },
+    },
+} );
 
 export default Footer
